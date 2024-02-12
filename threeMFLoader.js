4cbb2e07164c904b7af26cf496e771bccf99bdd5
Aayush Kokate <aayushext@outlook.com>
2024-02-11 22:04:43 -0800

    First Commit and added multiple model loaders

---
+++ threeMFLoader.js
@@ -0,0 +1,116 @@
+import * as THREE from 'three';
+
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { ThreeMFLoader } from 'three/addons/loaders/3MFLoader.js';
+
+let camera, scene, renderer;
+
+init();
+
+function init() {
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xa0a0a0);
+    scene.fog = new THREE.Fog(0xa0a0a0, 10, 2000);
+
+    camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 4000);
+    camera.position.set(-500, 40, 500);
+    scene.add(camera);
+
+    //
+
+    const hemiLight = new THREE.HemisphereLight(0xffffff, 0x8d8d8d, 3);
+    hemiLight.position.set(0, 2000, 0);
+    scene.add(hemiLight);
+
+    const dirLight = new THREE.DirectionalLight(0xffffff, 3);
+    dirLight.position.set(-0, 40, 50);
+    dirLight.castShadow = true;
+    dirLight.shadow.camera.top = 50;
+    dirLight.shadow.camera.bottom = - 25;
+    dirLight.shadow.camera.left = - 25;
+    dirLight.shadow.camera.right = 25;
+    dirLight.shadow.camera.near = 0.1;
+    dirLight.shadow.camera.far = 2000;
+    dirLight.shadow.mapSize.set(1024, 1024);
+    scene.add(dirLight);
+
+    // scene.add( new THREE.CameraHelper( dirLight.shadow.camera ) );
+
+    //
+
+    const manager = new THREE.LoadingManager();
+
+    const loader = new ThreeMFLoader(manager);
+    loader.load('./models/Kinetic-Sculpture.3MF', function (object) {
+
+        // object.rotation.set(- Math.PI / 2, 0, 0); // z-up conversion
+
+
+        object.traverse(function (child) {
+
+            child.castShadow = true;
+
+        });
+
+        scene.add(object);
+
+    });
+
+    //
+
+    manager.onLoad = function () {
+
+        render();
+
+    };
+
+    //
+
+    // const ground = new THREE.Mesh(new THREE.PlaneGeometry(1000, 1000), new THREE.MeshPhongMaterial({ color: 0xcbcbcb, depthWrite: false }));
+    // ground.rotation.x = - Math.PI / 2;
+    // ground.position.y = 11;
+    // ground.receiveShadow = true;
+    // scene.add(ground);
+
+    //
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    document.body.appendChild(renderer.domElement);
+
+    //
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.addEventListener('change', render);
+    controls.minDistance = 50;
+    controls.maxDistance = 1000;
+    controls.enablePan = false;
+    controls.target.set(0, 0, 0);
+    controls.update();
+
+    window.addEventListener('resize', onWindowResize);
+
+    render();
+
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    render();
+
+}
+
+function render() {
+
+    renderer.render(scene, camera);
+
+}
